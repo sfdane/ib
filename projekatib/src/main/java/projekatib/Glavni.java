6654596056eefa8840399c33570ddfa8fd5dafef
Dane OrliC <orlicdane96@gmail.com>
2019-05-19 21:05:20 +0200

    komit 1

---
+++ projekatib/src/main/java/projekatib/Glavni.java
@@ -0,0 +1,24 @@
+package projekatib;
+
+
+
+import java.io.File;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import controller.FileUploadController;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan({"demo","controller"})
+public class Glavni {
+
+	public static void main(String[] args) {
+		new File(FileUploadController.uploadDirectory).mkdir();
+		SpringApplication.run(Glavni.class, args);
+	}
+}
